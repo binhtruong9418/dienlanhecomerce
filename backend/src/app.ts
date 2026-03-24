849467ed5674320ba45362866c3fa04543b49845
blackhegde <hoangkyanhyb1@gmail.com>
2026-03-24 10:33:24 +0700

    be port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -101,7 +101,7 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
